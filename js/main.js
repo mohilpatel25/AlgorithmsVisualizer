ae22766cdf8d3f22acc9a5ff53d33ffbb932251f
mohilpatel25 <mohilpatel25@users.noreply.github.com>
2020-12-15 16:32:00 +0530

    selection

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function load(){
   bubble_generate();
-  insert_generate();
+  select_generate();
 }
 
 //=========================================bubble sort=============================================================================
@@ -46,33 +46,33 @@ async function bubble_sort(count){
   document.getElementById('bubble_sort_button').disabled = false;
 }
 
-//=========================================insertion sort=============================================================================
-function insert_generate(count=50){
-  document.getElementById('insert_chart').style.gridTemplateColumns= `repeat(${count}, 1fr)`;
+//=========================================selection sort=============================================================================
+function select_generate(count=50){
+  document.getElementById('select_chart').style.gridTemplateColumns= `repeat(${count}, 1fr)`;
   var bars = "";
   for(let i=0;i<count;i++){
-    bars += "<div id='insert_bar_"+i+"' class='bars'></div>\n";
+    bars += "<div id='select_bar_"+i+"' class='bars'></div>\n";
   }
-  document.getElementById('insert_chart').innerHTML = "";
-  document.getElementById('insert_chart').innerHTML = bars;
+  document.getElementById('select_chart').innerHTML = "";
+  document.getElementById('select_chart').innerHTML = bars;
   for(let i=0;i<count;i++){
     let h = Math.floor(Math.random() * 90) + 2;
-    document.getElementById('insert_bar_'+i).style.gridRowStart = `${h}`;
+    document.getElementById('select_bar_'+i).style.gridRowStart = `${h}`;
   }
 }
 
-async function insert_sort(count){
-  document.getElementById('insert_slider').disabled = true;
-  document.getElementById('insert_generate_button').disabled = true;
-  document.getElementById('insert_sort_button').disabled = true;
+async function select_sort(count){
+  document.getElementById('select_slider').disabled = true;
+  document.getElementById('select_generate_button').disabled = true;
+  document.getElementById('select_sort_button').disabled = true;
   
   for(let i=0;i<count;i++){
-    let bar1 = document.getElementById('insert_bar_'+i)
+    let bar1 = document.getElementById('select_bar_'+i)
     bar1.style.backgroundColor = "#DC143C";
     let v1 = parseInt(bar1.style.gridRowStart);
     let minval = v1, minind = i;
     for(let j=i;j<count;j++){
-      let bar2 = document.getElementById('insert_bar_'+j)
+      let bar2 = document.getElementById('select_bar_'+j)
       bar2.style.backgroundColor = "#DC143C";
       let v2 = parseInt(bar2.style.gridRowStart);
       await sleep(10);
@@ -83,13 +83,13 @@ async function insert_sort(count){
       bar2.style.backgroundColor = "#4B0082";
     }
     bar1.style.gridRowStart = minval;
-    document.getElementById('insert_bar_'+minind).style.gridRowStart = v1;
+    document.getElementById('select_bar_'+minind).style.gridRowStart = v1;
     bar1.style.backgroundColor = "#4B0082";
   }
 
-  document.getElementById('insert_slider').disabled = false;
-  document.getElementById('insert_generate_button').disabled = false;
-  document.getElementById('insert_sort_button').disabled = false;
+  document.getElementById('select_slider').disabled = false;
+  document.getElementById('select_generate_button').disabled = false;
+  document.getElementById('select_sort_button').disabled = false;
 }
 
 //sleep function
